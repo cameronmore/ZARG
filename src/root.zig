115ad6c4730dfcee80704b8536474496e32c7827
cameronmore <camerongmore@gmail.com>
2025-06-28 09:09:14 -0400

    removed autogen func

--- src/root.zig
+++ src/root.zig
@@ -174,11 +174,3 @@ pub const argManager = struct {
         return;
     }
 };
-
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
